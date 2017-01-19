43806022270a812aedc3a14333115d4964f3643c
faxianbar <faxianbar@faxianbardeiMac.local>
2017-01-19 09:06:59 +0800

    Initial Commit

---
+++ Discovery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Discovery
+//
+//  Created by faxianbar on 2017/1/19.
+//  Copyright © 2017年 faxianbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Discovery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Discovery
+//
+//  Created by faxianbar on 2017/1/19.
+//  Copyright © 2017年 faxianbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
